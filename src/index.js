2337ab68068c980b33150dbf224e5ffebe220baf
Pasha <20466125+Pasha19@users.noreply.github.com>
2018-07-25 16:35:14 +0300

    eslint

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 
 const div = document.createElement('div')
-document.body.appendChild(div);
+document.body.appendChild(div)
 
+/* eslint-disable-next-line no-new */
 new Vue({
     el: div,
-    render: h => h(App),
+    render: h => h(App)
 })
