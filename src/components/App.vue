6a23eea8a56d9d0d75b459bec246cbd288523a8e
Pasha <20466125+Pasha19@users.noreply.github.com>
2018-07-25 17:28:08 +0300

    vue router

---
+++ src/components/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <div>
+            <router-link to="/">Home</router-link>
+            <router-link to="/foo">Foo</router-link>
+            <router-link to="/bar">Bar</router-link>
+        </div>
+        <div>
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
+import App from './components/App.vue'
+import Home from './components/Home.vue'
+import Bar from './components/Bar.vue'
+import Foo from './components/Foo.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+    { path: '', component: Home },
+    { path: '/foo', component: Foo },
+    { path: '/bar', component: Bar },
+]
+const router = new VueRouter({
+    routes,
+    mode: 'history',
+})
 
 const div = document.createElement('div')
 document.body.appendChild(div)
@@ -7,5 +23,6 @@ document.body.appendChild(div)
 /* eslint-disable-next-line no-new */
 new Vue({
     el: div,
+    router,
     render: h => h(App)
 })
