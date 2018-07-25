572e640910d1b0d86a9862656dee1ff4d3cbbebc
Pasha <20466125+Pasha19@users.noreply.github.com>
2018-07-25 15:42:33 +0300

    event source polyfill installed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello')
+import 'event-source-polyfill'
+
+let es = new EventSourcePolyfill(
+    '/api/chat/listen',
+    {
+        headers: {
+            'Authorization': 'TOKEN',
+        },
+    },
+)
+es.addEventListener("message", console.log)
